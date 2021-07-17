d9fcfd41e0411f3c7c8f59462745aabb3dd348aa
Stevellen <belcher.steven@gmail.com>
2021-07-17 15:25:10 -0500

    Improving additional page search
    Now using the href provided by the site's next arrow rather than manually replacing a substring, as that approach was found to be unreliable.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Inventory checker for BestBuy.
 
 ## To use
 
-1. Install python 3.8 from [here](https://www.python.org/downloads/release/python-380/) - make sure to select Add to Path checkbox
+1. Install python 3.8+ from [here](https://www.python.org/downloads/release/python-380/) - make sure to select Add to Path checkbox
 2. Copy or clone the repository
 3. Open a terminal/cmd and navigate to the folder containing inventory_check.py
 4. run `python -m pip install -r requirements.txt`
--- inventory_check.py
+++ inventory_check.py
@@ -1,10 +1,18 @@
-""""""
+"""
+Created by Steven Belcher
+
+TODO:
+    - Add email and SMS updates for users
+"""
+import os
+import sys
 import argparse
 import requests
 import webbrowser
 from time import sleep
 from random import randint
 from urllib.parse import urljoin
+from twilio.rest import Client
 from bs4 import BeautifulSoup as BS
 
 # DEFINING CONSTANTS
@@ -15,6 +23,7 @@ HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 # set to hold found items so they aren't opened repeatedly
 TOUCHED = set()
 
+
 parser = argparse.ArgumentParser()
 
 def get_products(soup):
@@ -22,41 +31,38 @@ def get_products(soup):
 
 def check_stock(products):
     global TOUCHED
-    for i, item in enumerate(products):
-        # if the product isn't sold out and we haven't already TOUCHED it, open a window/tab
+    for item in products:
         button = item.find('button', {"class": "add-to-cart-button"})
         item_id = item.attrs.get("data-sku-id")
         if button and (button.attrs.get('data-button-state') != 'SOLD_OUT'):
             e = item.h4.a.attrs.get('href')
             if e and (item_id not in TOUCHED):
+                # join url, add itemid to the set of visited ids, open a browser tab
                 prod_page = urljoin(HOMEPAGE, e)
                 TOUCHED |= {item_id}
-
-                if i == 0:
-                    webbrowser.open_new(prod_page)
-                else:
-                    webbrowser.open_new_tab(prod_page)
+                print(f"Opening browser tab for {item.h4.a.string}!")
+                webbrowser.open_new_tab(prod_page)
 
 def search_page(url, headers):
     res = requests.get(url, headers=headers)
-    assert res.ok, f"Got response {res.status_code} from server!"
-    soup = BS(res.text, 'html.parser')    
-    
+    assert res.ok, f"""Got response {res.status_code} from server!
+        Trying URL {url}"""
+    # check first page for products
+    soup = BS(res.text, 'html.parser')
     products = get_products(soup)
     check_stock(products)
     
     # check subsequent pages
-    pages = soup.find('ol', {'class': 'paging-list'})
-    if pages is not None:
-        for i in range(1, len(pages)):
-            next_url = url.replace('jsp?', f'jsp?cp={i+1}')
-            res = requests.get(next_url, headers=headers)
-            assert res.ok, f"Got response {res.status_code} from server!"
-            soup = BS(res.text, 'html.parser')
-            products = get_products(soup)
+    while next_page := soup.find('a', {'class': 'sku-list-page-next'}):
+        if next_page.attrs.get('aria-disabled') == 'true':
+            break
+        if next_url := next_page.attrs.get('href'):
+            products = get_products(
+                soup := BS(requests.get(next_url, headers=headers).text, 'html.parser')
+            )
             check_stock(products)
 
-            
+
 def main(URL):
     global TOUCHED
     global HEADERS
@@ -66,9 +72,10 @@ def main(URL):
         if len(TOUCHED) == 0:
             print("No inventory found, trying again in a few seconds...")
         else:
-            print("Found the following item(s) in stock:")
+            print("Found the following sku(s) in stock:")
             for item in TOUCHED:
                 print(f"\t{item}")
+            print("Checking for new inventory in a few seconds...")
 
         sleep(randint(5,10))
         
