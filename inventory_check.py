b28ba5571b645fdba68df280a3eb7ca049c28cc4
Stevellen <belcher.steven@gmail.com>
2021-07-19 10:19:09 -0500

    Small Updated
    Added timestamp to console output so it's clear that the script is still running. Added an --in_store flag to indicate whether to include items listed as "In Store Only" in the search.

--- inventory_check.py
+++ inventory_check.py
@@ -11,8 +11,9 @@ import requests
 import webbrowser
 from time import sleep
 from random import randint
-from urllib.parse import urljoin
+from datetime import datetime
 from twilio.rest import Client
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup as BS
 
 # DEFINING CONSTANTS
@@ -29,12 +30,13 @@ parser = argparse.ArgumentParser()
 def get_products(soup):
     return soup.find_all('li', {"class": "sku-item"})
 
-def check_stock(products):
+def check_stock(products, in_store):
     global TOUCHED
+    invalid_status = ['SOLD_OUT'] if in_store else ['SOLD_OUT', 'IN_STORE_ONLY']
     for item in products:
         button = item.find('button', {"class": "add-to-cart-button"})
         item_id = item.attrs.get("data-sku-id")
-        if button and (button.attrs.get('data-button-state') != 'SOLD_OUT'):
+        if button and (button.attrs.get('data-button-state') not in invalid_status):
             e = item.h4.a.attrs.get('href')
             if e and (item_id not in TOUCHED):
                 # join url, add itemid to the set of visited ids, open a browser tab
@@ -43,14 +45,14 @@ def check_stock(products):
                 print(f"Opening browser tab for {item.h4.a.string}!")
                 webbrowser.open_new_tab(prod_page)
 
-def search_page(url, headers):
+def search_page(url, headers, in_store):
     res = requests.get(url, headers=headers)
     assert res.ok, f"""Got response {res.status_code} from server!
         Trying URL {url}"""
     # check first page for products
     soup = BS(res.text, 'html.parser')
     products = get_products(soup)
-    check_stock(products)
+    check_stock(products, in_store)
     
     # check subsequent pages
     while next_page := soup.find('a', {'class': 'sku-list-page-next'}):
@@ -60,19 +62,20 @@ def search_page(url, headers):
             products = get_products(
                 soup := BS(requests.get(next_url, headers=headers).text, 'html.parser')
             )
-            check_stock(products)
+            check_stock(products, in_store)
 
 
-def main(URL):
+def main(url, in_store):
     global TOUCHED
     global HEADERS
     # main loop
     while True:
-        search_page(URL, HEADERS)
+        search_page(url, HEADERS, in_store)
+        dt = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         if len(TOUCHED) == 0:
-            print("No inventory found, trying again in a few seconds...")
+            print(dt, "No inventory found, trying again in a few seconds...")
         else:
-            print("Found the following sku(s) in stock:")
+            print(dt, "Found the following sku(s) in stock:")
             for item in TOUCHED:
                 print(f"\t{item}")
             print("Checking for new inventory in a few seconds...")
@@ -82,8 +85,9 @@ def main(URL):
 
 if __name__ == "__main__":
     parser.add_argument("url", help="BestBuy URL to search", type=str)
+    parser.add_argument("--in_store", help="Whether to notify about products listed as In Store Only", default=False, action="store_true")
     args = parser.parse_args()
-    main(args.url)
+    main(args.url, args.in_store)
 
 
 
