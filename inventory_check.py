050301beb56b5c9097b48b3ed9c48978b32953ff
Stevellen <belcher.steven@gmail.com>
2021-07-23 17:02:33 -0500

    Updaaaates

--- inventory_check.py
+++ inventory_check.py
@@ -23,15 +23,29 @@ HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 
 # set to hold found items so they aren't opened repeatedly
 TOUCHED = set()
+NOTIFIED = set()
 
 
 parser = argparse.ArgumentParser()
+
+
+def get_soup(url, headers, timeout=10):
+    try:
+        res = requests.get(url, headers=headers, timeout=timeout)
+        assert res.ok, f"""Got response {res.status_code} from server!"""
+        return BS(res.text, 'html.parser')
+    except (TimeoutError, Exception) as e:
+        # Sometimes we timeout. Not sure why, but all we do is print the error and try again in a few seconds
+        print("Error encountered!")
+        print(e)
+        return None
 
 def get_products(soup):
     return soup.find_all('li', {"class": "sku-item"})
 
 def check_stock(products, in_store):
     global TOUCHED
+    global NOTIFIED
     invalid_status = ['SOLD_OUT'] if in_store else ['SOLD_OUT', 'IN_STORE_ONLY']
     for item in products:
         item_id = item.attrs.get("data-sku-id")
@@ -39,21 +53,19 @@ def check_stock(products, in_store):
         if button is None:
             button = item.find('a', {"class": "add-to-cart-button"})
 
-        if button and (button.attrs.get('data-button-state') not in invalid_status):
+        if button and ((status := button.attrs.get('data-button-state')) not in invalid_status):
             e = item.h4.a.attrs.get('href')
-            if e and (item_id not in TOUCHED):
+            if e and ((item_id, status) not in NOTIFIED):
                 # join url, add itemid to the set of visited ids, open a browser tab
                 prod_page = urljoin(HOMEPAGE, e)
-                TOUCHED |= {item_id}
+                TOUCHED |= {(item_id, status)}
                 print(f"Opening browser tab for {item.h4.a.string}!")
                 webbrowser.open_new_tab(prod_page)
 
 def search_page(url, headers, in_store):
-    res = requests.get(url, headers=headers)
-    assert res.ok, f"""Got response {res.status_code} from server!
-        Trying URL {url}"""
-    # check first page for products
-    soup = BS(res.text, 'html.parser')
+    soup = get_soup(url, headers)
+    if soup is None:
+        return
     products = get_products(soup)
     check_stock(products, in_store)
     
@@ -62,14 +74,14 @@ def search_page(url, headers, in_store):
         if next_page.attrs.get('aria-disabled') == 'true':
             break
         if next_url := next_page.attrs.get('href'):
-            products = get_products(
-                soup := BS(requests.get(next_url, headers=headers).text, 'html.parser')
-            )
+            soup = get_soup(next_url, headers)
+            products = get_products(soup)
             check_stock(products, in_store)
 
 
 def main(url, in_store):
     global TOUCHED
+    global NOTIFIED
     global HEADERS
     # main loop
     while True:
@@ -78,11 +90,13 @@ def main(url, in_store):
         if len(TOUCHED) == 0:
             print(dt, "No inventory found, trying again in a few seconds...")
         else:
+            NOTIFIED |= TOUCHED
             print(dt, "Found the following sku(s) in stock:")
-            for item in TOUCHED:
-                print(f"\t{item}")
+            for (itemid, status) in TOUCHED:
+                print(f"\t{itemid} - {status}")
+            TOUCHED = set()
             print("Checking for new inventory in a few seconds...")
-
+        # wait a bit so the server doesn't kick us out
         sleep(randint(5,10))
         
 
